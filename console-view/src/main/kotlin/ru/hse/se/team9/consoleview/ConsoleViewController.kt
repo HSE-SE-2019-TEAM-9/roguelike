05cecdd8fc69e06e675e8e72fb54704c43e42c73
Konstantin Lyubort <lyubortk@gmail.com>
2020-04-26 01:29:26 +0300

    change application title

--- console-view/src/main/kotlin/ru/hse/se/team9/consoleview/ConsoleViewController.kt
+++ console-view/src/main/kotlin/ru/hse/se/team9/consoleview/ConsoleViewController.kt
@@ -150,7 +150,7 @@ class ConsoleViewController(private val width: Int = 150, private val height: In
     }
 
     companion object {
-        private const val APP_TITLE = "Roguelike-1"
+        private const val APP_TITLE = "Roguelike-2"
         private const val CHOOSE_FILE_TITLE = "Choose file"
         private const val ERROR_TITLE = "Error"
     }
