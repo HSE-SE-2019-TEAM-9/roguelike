a689bb4820f4f917a879edea548dbffbd3ed3810
skuzi <serg.kuzmin00@mail.ru>
2020-04-29 00:24:18 +0300

    optimize imports in consumable

--- model/src/main/kotlin/ru/hse/se/team9/game/entities/hero/consumables/Consumable.kt
+++ model/src/main/kotlin/ru/hse/se/team9/game/entities/hero/consumables/Consumable.kt
@@ -1,9 +1,6 @@
 package ru.hse.se.team9.game.entities.hero.consumables
 
-import ru.hse.se.team9.game.entities.hero.Hero
 import ru.hse.se.team9.game.entities.hero.effects.DeltaHpEffect
-import ru.hse.se.team9.game.entities.hero.effects.Effect
-import kotlin.math.min
 
 class Consumable(private val hpGain: Int = 0) {
     fun getEffect() =  DeltaHpEffect(hpGain)
