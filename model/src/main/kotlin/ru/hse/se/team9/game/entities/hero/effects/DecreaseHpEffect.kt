4e22d99e0f7a0060eb76f26eeb7a22a0edb449de
Maria Malysheva <mn.malysheva@mail.ru>
2020-04-28 22:02:04 +0300

    hp cannot be negative

--- model/src/main/kotlin/ru/hse/se/team9/game/entities/hero/effects/DecreaseHpEffect.kt
+++ model/src/main/kotlin/ru/hse/se/team9/game/entities/hero/effects/DecreaseHpEffect.kt
@@ -1,10 +1,11 @@
 package ru.hse.se.team9.game.entities.hero.effects
 
 import ru.hse.se.team9.game.entities.hero.Hero
+import kotlin.math.max
 
 /** Decreases hero health points */
 class DecreaseHpEffect(private val number: Int) : Effect {
     override fun invoke(hero: Hero) {
-        hero.stats.hp -= number
+        hero.stats.hp = max(hero.stats.hp - number, 0)
     }
 }
