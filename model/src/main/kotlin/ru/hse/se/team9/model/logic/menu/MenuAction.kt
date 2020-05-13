3585f39ef7719b73e769deb87c2ab68ee70f377a
skuzi <serg.kuzmin00@mail.ru>
2020-05-13 05:11:51 +0300

    add some ui for multiplayer

--- model/src/main/kotlin/ru/hse/se/team9/model/logic/menu/MenuAction.kt
+++ model/src/main/kotlin/ru/hse/se/team9/model/logic/menu/MenuAction.kt
@@ -4,10 +4,16 @@ package ru.hse.se.team9.model.logic.menu
  * Corresponds to all player action in menu.
  */
 sealed class MenuAction
-sealed class StartGame : MenuAction()
-object NewGame : StartGame()
-object LoadSavedGame : StartGame()
-object OpenGameFromFile : StartGame()
+sealed class StartLocalGame : MenuAction()
+object NewLocalGame : StartLocalGame()
+object LoadSavedGame : StartLocalGame()
+object OpenGameFromFile : StartLocalGame()
+object StartOnlineGame: MenuAction()
+
+sealed class OnlineMenuAction : MenuAction()
+object BackToLocalMenu: OnlineMenuAction()
+object JoinExistingSession: OnlineMenuAction()
+object CreateSession: OnlineMenuAction()
 
 object Continue : MenuAction()
 object Save : MenuAction()
