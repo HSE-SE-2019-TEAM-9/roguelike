7afdbdddff190261a77c3b0570f31e308d45eb3f
Maria Malysheva <mn.malysheva@mail.ru>
2020-05-13 05:03:23 +0300

    add server

---
+++ model/src/main/kotlin/ru/hse/se/team9/network/RoguelikeServer.kt
@@ -0,0 +1,189 @@
+package ru.hse.se.team9.network
+
+import arrow.core.getOrHandle
+import com.google.protobuf.Empty
+import io.grpc.ServerBuilder
+import io.grpc.stub.StreamObserver
+import ru.hse.se.team9.conversions.ToProtoConverter.toProto
+import ru.hse.se.team9.conversions.FromProtoConverter.toView
+import ru.hse.se.team9.entities.views.MapView
+import ru.hse.se.team9.game.entities.map.distance.Manhattan
+import ru.hse.se.team9.model.generators.GameGenerator
+import ru.hse.se.team9.model.generators.confusion.RandomStrategyModifier
+import ru.hse.se.team9.model.generators.consumables.RandomConsumable
+import ru.hse.se.team9.model.generators.directions.RandomDirection
+import ru.hse.se.team9.model.generators.heroes.DefaultHeroCreator
+import ru.hse.se.team9.model.generators.items.RandomItem
+import ru.hse.se.team9.model.generators.mobs.RandomMob
+import ru.hse.se.team9.model.generators.positions.RandomPosition
+import ru.hse.se.team9.model.logic.gamecycle.*
+import ru.hse.se.team9.model.mapgeneration.creators.RandomMapCreator
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.LinkedBlockingQueue
+import java.util.concurrent.TimeUnit
+
+class RoguelikeServer : RoguelikeApiGrpc.RoguelikeApiImplBase() {
+    private val games = ConcurrentHashMap<Int, GameSession>()
+    private var sessionCounter = 0
+    private val generator = GameGenerator(
+        RandomDirection,
+        RandomPosition,
+        RandomMob(RandomDirection),
+        RandomStrategyModifier(RandomDirection),
+        RandomItem,
+        RandomConsumable,
+        DefaultHeroCreator
+    )
+    private val mapCreator = RandomMapCreator.build(generator, distance = Manhattan, fogRadius = 10)
+        .getOrHandle { throw it } // Never happens
+
+    override fun getGames(request: Empty, responseObserver: StreamObserver<Service.GetGamesResponse>) {
+        responseObserver.onNext(getGameSessions())
+        responseObserver.onCompleted()
+    }
+
+    override fun join(responseObserver: StreamObserver<Service.ServerMessage>)
+            : StreamObserver<Service.PlayerMessage> {
+        return PlayerActionHandler(responseObserver)
+    }
+
+    @Synchronized
+    private fun getGameSessions(): Service.GetGamesResponse {
+        return Service.GetGamesResponse.newBuilder().addAllGames(
+            games.values.map { it.getInfo() }
+        ).build()
+    }
+
+    @Synchronized
+    private fun addGameSession(): Int {
+        val gameId = sessionCounter++
+        val gameSession = GameSession(gameId, gameId.toString())
+        Thread(gameSession).start()
+        games[gameId] = gameSession
+        return gameId
+    }
+
+    private inner class GameSession(private val id: Int, private val name: String) : Runnable {
+        private val game = GameCycleLogicImpl(mapCreator.createMap().getOrHandle { throw it }, generator)
+        private var playerCounter = 0
+        private val players = ConcurrentHashMap<Int, PlayerActionHandler>()
+        private val playerActions = LinkedBlockingQueue<PlayerActionId>()
+
+        @Synchronized
+        fun getInfo(): Service.GameInfo {
+            return Service.GameInfo.newBuilder()
+                .setGameId(id)
+                .setName(name)
+                .setPlayers(playerCounter) // FIXME
+                .build()
+        }
+
+        @Synchronized
+        fun addPlayerAction(playerAction: PlayerActionId) {
+            playerActions.add(playerAction)
+        }
+
+        @Synchronized
+        fun addPlayer(player: PlayerActionHandler): Int {
+            val playerId = playerCounter++
+            game.map.addHeroToRandomPosition(playerId, DefaultHeroCreator.createHero())
+            players[playerId] = player
+            return playerId
+        }
+
+        @Synchronized
+        fun processPlayerAction() {
+            val playerAction = playerActions.poll(30, TimeUnit.HOURS)!! // FIXME
+            val action = playerAction.action
+            val id = playerAction.id
+            when (action.requestCase) {
+                Service.PlayerMessage.RequestCase.MAKE_MOVE -> {
+                    game.makeMove(id, unpackMove(action.makeMove.move))
+                    for (player in players.values) {
+                        player.out.onNext(packMap(game.getCurrentMap(player.heroId)))
+                    }
+                }
+                Service.PlayerMessage.RequestCase.GET_CURRENT_MAP -> {
+                    val player = players[id]!!
+                    player.out.onNext(packMap(game.getCurrentMap(player.heroId)))
+                }
+                Service.PlayerMessage.RequestCase.PUT_ON_ITEM -> {
+                    game.putOnItem(id, action.putOnItem.index)
+                    val player = players[id]!!
+                    player.out.onNext(packMap(game.getCurrentMap(player.heroId)))
+                }
+                Service.PlayerMessage.RequestCase.PUT_OFF_ITEM -> {
+                    game.putOffItem(id, action.putOffItem.type.toView())
+                    val player = players[id]!!
+                    player.out.onNext(packMap(game.getCurrentMap(player.heroId)))
+                }
+                else -> throw IllegalStateException()
+            }
+        }
+
+        override fun run() {
+            while (true) {
+                processPlayerAction()
+            }
+        }
+    }
+
+    private inner class PlayerActionHandler(val out: StreamObserver<Service.ServerMessage>)
+            : StreamObserver<Service.PlayerMessage> {
+        var heroId = -1
+        var gameId = -1
+        var gameSession: GameSession? = null
+
+        override fun onNext(value: Service.PlayerMessage) {
+            gameSession?.addPlayerAction(PlayerActionId(value, heroId)) ?: let {
+                when (value.requestCase!!) {
+                    Service.PlayerMessage.RequestCase.JOIN_GAME -> {
+                        gameId = value.joinGame.gameId
+                        gameSession = games[gameId]
+                        heroId = gameSession!!.addPlayer(this)
+                    }
+                    Service.PlayerMessage.RequestCase.START_GAME -> {
+                        gameId = addGameSession()
+                        gameSession = games[gameId]
+                        heroId = gameSession!!.addPlayer(this)
+                    }
+                    else -> throw IllegalStateException()
+                }
+            }
+        }
+
+        override fun onError(t: Throwable) {
+        }
+
+        override fun onCompleted() {
+        }
+    }
+
+    private class PlayerActionId(val action: Service.PlayerMessage, val id: Int)
+
+    private fun packMap(mapView: MapView) =
+        Service.ServerMessage.newBuilder()
+            .setMapUpdate(
+                Service.ServerMessage.MapUpdate.newBuilder()
+                    .setMap(mapView.toProto())
+                    .build()
+            ).build()
+
+    fun unpackMove(move: Service.PlayerMessage.MakeMove.Move): Move {
+        return when (move) {
+            Service.PlayerMessage.MakeMove.Move.LEFT -> Left
+            Service.PlayerMessage.MakeMove.Move.UP -> Up
+            Service.PlayerMessage.MakeMove.Move.RIGHT -> Right
+            Service.PlayerMessage.MakeMove.Move.DOWN -> Down
+            Service.PlayerMessage.MakeMove.Move.UNRECOGNIZED -> error("deserialization error")
+        }
+    }
+
+    companion object {
+        fun run(port: Int) {
+            val serverImpl = RoguelikeServer()
+            val server = ServerBuilder.forPort(port).addService(serverImpl).build()
+            server.start()
+        }
+    }
+}
